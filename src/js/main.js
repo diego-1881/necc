edde4bde6581acda5498bbca8bdd132a6f448404
Diego Esteban <desteban@necc.org>
2024-11-19 06:32:21 -0500

    separate nav and footer, add card scss base

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,14 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  async function loadHTML(elementId, url) {
+    const container = document.getElementById(elementId);
+    const response = await fetch(url);
+    const html = await response.text();
+    container.innerHTML = html;
+  }
+
+  await loadHTML("footer", "footer.html");
+  await loadHTML("topNav", "nav.html");
+});
