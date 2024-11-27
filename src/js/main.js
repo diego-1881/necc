c17d6f55e6fcbf6b2da7c0dc917691afe0c732b6
Diego Esteban <desteban@necc.org>
2024-11-27 08:07:18 -0500

    add Our Leadership page

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,35 @@ document.addEventListener("DOMContentLoaded", async () => {
   await loadHTML("footer", "footer.html");
   await loadHTML("topNav", "nav.html");
 });
+
+// Our leadership team
+// Disable the cards that are not selected
+function disableUnselectedCards() {
+  const cards = document.querySelectorAll(".team-accordion.card-team");
+
+  cards.forEach((card) => {
+    card.addEventListener("click", () => {
+      const isSelected = card.classList.contains("selected");
+
+      cards.forEach((c) => {
+        c.classList.remove("selected", "opacity-50", "pe-none");
+      });
+
+      if (!isSelected) {
+        card.classList.add("selected");
+        cards.forEach((c) => {
+          if (!c.classList.contains("selected")) {
+            c.classList.add("opacity-50", "pe-none");
+          }
+        });
+      }
+    });
+  });
+}
+
+function init() {
+  document.addEventListener("DOMContentLoaded", async () => {
+    disableUnselectedCards();
+  });
+}
+init();
